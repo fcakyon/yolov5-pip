0d458bce1713895edc094ef98b1864b68fa0d1f5
fcakyon <fcakyon@gmail.com>
2021-02-12 00:11:08 +0300

    update to v0.7

--- yolov5/__init__.py
+++ yolov5/__init__.py
@@ -1,10 +1,6 @@
-from yolov5.data import *
 from yolov5.detect import detect
 from yolov5.helpers import YOLOv5, load_model
-from yolov5.models import *
 from yolov5.test import test
 from yolov5.train import train
-from yolov5.utils import *
-from yolov5.weights import *
 
-__version__ = "0.6"
+__version__ = "0.7"
