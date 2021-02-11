8dad604365836d3d20285392e123c64235611ee6
fcakyon <fcakyon@gmail.com>
2021-02-12 00:10:57 +0300

    add more tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,5 @@
 class TestConstants:
     YOLOV5S_MODEL_PATH = "yolov5/weights/yolov5s.pt"
+    YOLOV5L_MODEL_PATH = "yolov5/weights/yolov5l.pt"
     ZIDANE_IMAGE_PATH = "tests/data/zidane.jpg"
+    BUS_IMAGE_PATH = "tests/data/bus.jpg"
--- tests/test_yolov5.py
+++ tests/test_yolov5.py
@@ -1,17 +1,13 @@
-# OBSS SAHI Tool
-# Code written by Fatih C Akyon, 2020.
-
 import unittest
-
-import numpy as np
-from PIL import Image
-from yolov5 import YOLOv5
 
 from tests.test_utils import TestConstants
 
 
 class TestYolov5(unittest.TestCase):
     def test_load_model(self):
+        from PIL import Image
+        from yolov5 import YOLOv5
+
         # init model
         model_path = TestConstants.YOLOV5S_MODEL_PATH
         device = "cpu"
@@ -22,6 +18,9 @@ class TestYolov5(unittest.TestCase):
         self.assertNotEqual(yolov5.model, None)
 
     def test_load_model_on_init(self):
+        from PIL import Image
+        from yolov5 import YOLOv5
+
         # init model
         model_path = TestConstants.YOLOV5S_MODEL_PATH
         device = "cpu"
@@ -31,8 +30,10 @@ class TestYolov5(unittest.TestCase):
         self.assertNotEqual(yolov5.model, None)
 
     def test_predict(self):
+        from PIL import Image
+        from yolov5 import YOLOv5
 
-        # init model
+        # init yolov5s model
         model_path = TestConstants.YOLOV5S_MODEL_PATH
         device = "cpu"
         yolov5 = YOLOv5(model_path, device, load_on_init=True)
@@ -42,12 +43,51 @@ class TestYolov5(unittest.TestCase):
         image = Image.open(image_path)
 
         # perform inference
-        results = yolov5.predict(image, augment=False)
+        results = yolov5.predict(image, size=640, augment=False)
 
         # compare
-        self.assertEqual(box[:4].astype("int").tolist(), [336, 123, 346, 139])
-        self.assertEqual(len(boxes), 80)
-        self.assertEqual(len(masks), 80)
+        self.assertEqual(results.n, 1)
+        self.assertEqual(len(results.names), 80)
+        self.assertEqual(len(results.pred[0]), 3)
+        
+        # init yolov5l model
+        model_path = TestConstants.YOLOV5L_MODEL_PATH
+        device = "cpu"
+        yolov5 = YOLOv5(model_path, device, load_on_init=True)
+
+        # prepare image
+        image_path = TestConstants.BUS_IMAGE_PATH
+        image = Image.open(image_path)
+        image_path = "/home/fatihakyon/dev/obss/sahi/tests/data/small-vehicles1.jpeg"
+        # perform inference
+        results = yolov5.predict(image, size=1280, augment=False)
+
+        # compare
+        self.assertEqual(results.n, 1)
+        self.assertEqual(len(results.names), 80)
+        self.assertEqual(len(results.pred[0]), 26)
+
+        # init yolov5s model
+        model_path = TestConstants.YOLOV5S_MODEL_PATH
+        device = "cpu"
+        yolov5 = YOLOv5(model_path, device, load_on_init=True)
+
+        # prepare images
+        image_path1 = TestConstants.ZIDANE_IMAGE_PATH
+        image_path2 = TestConstants.BUS_IMAGE_PATH
+        image1 = Image.open(image_path1)
+        image2 = Image.open(image_path2)
+
+        # perform inference with multiple images and test augmentation
+        results = yolov5.predict([image1, image2], size=1280, augment=True)
+
+        # compare
+        self.assertEqual(results.n, 2)
+        self.assertEqual(len(results.names), 80)
+        self.assertEqual(len(results.pred[0]), 5)
+        self.assertEqual(len(results.pred[1]), 5)
+        
+
 
 
 if __name__ == "__main__":
