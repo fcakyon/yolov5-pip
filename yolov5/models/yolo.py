d1c36e8a824734e03f9b17f3eb40bbf9bbe77813
fcakyon <34196005+fcakyon@users.noreply.github.com>
2021-03-24 23:10:55 +0300

    Update yolo.py

--- yolov5/models/yolo.py
+++ yolov5/models/yolo.py
@@ -79,7 +79,7 @@ class Detect(nn.Module):
 
 class Model(nn.Module):
     def __init__(
-        self, cfg="yolov5s.yaml", ch=3, nc=None, verbose=1
+        self, cfg="yolov5s.yaml", ch=3, nc=None, verbose=1, anchors=None
     ):  # model, input channels, number of classes
         super(Model, self).__init__()
         if isinstance(cfg, dict):
@@ -94,10 +94,11 @@ class Model(nn.Module):
         # Define model
         ch = self.yaml["ch"] = self.yaml.get("ch", ch)  # input channels
         if nc and nc != self.yaml["nc"]:
-            logger.info(
-                "Overriding model.yaml nc=%g with nc=%g" % (self.yaml["nc"], nc)
-            )
+            logger.info(f"Overriding model.yaml nc={self.yaml['nc']} with nc={nc}")
             self.yaml["nc"] = nc  # override yaml value
+        if anchors:
+            logger.info(f'Overriding model.yaml anchors with anchors={anchors}')
+            self.yaml['anchors'] = round(anchors)  # override yaml value
         self.model, self.save = parse_model(
             deepcopy(self.yaml), ch=[ch], verbose=verbose
         )  # model, savelist
