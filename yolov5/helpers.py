a91d0b093307b2f850a6b611381f356c00fcc605
kadirnar <kadir.nar@hotmail.com>
2022-06-05 22:27:38 +0300

    video_predict parametre düzenlendi.

--- yolov5/helpers.py
+++ yolov5/helpers.py
@@ -63,8 +63,7 @@ class YOLOv5:
         results = self.model(imgs=image_list, size=size, augment=augment)
         return results
 
-    def video_predict(self, video_path, view_img=True, img_size=640):
-        from yolov5.utils.datasets import LoadImages
+    def video_predict(self, video_path, view_img=True, size=640, augment=False, line_thickness=3):
         from yolov5.utils.general import non_max_suppression
         from utils.plots import Annotator, colors
         import cv2
@@ -75,11 +74,11 @@ class YOLOv5:
             _, im = cap.read()
             im0s = im.copy()
             im = torch.from_numpy(im).permute(2, 0, 1).float().div(255.0).unsqueeze(0)
-            pred = self.model(im)
-            pred = non_max_suppression(pred, conf_thres=0.25, iou_thres=0.45, classes=None, agnostic=False)
+            pred = self.model(im, size=size, augment=augment) 
+            pred = non_max_suppression(pred, conf_thres=self.model.conf, iou_thres=self.model.iou, classes=self.model.classes, agnostic=self.model.agnostic)
             for _, det in enumerate(pred):
                 for *xyxy, conf, cls in reversed(det):
-                    annotator = Annotator(im0s, line_width=3, example=str(self.model.names))
+                    annotator = Annotator(im0s, line_width=line_thickness, example=str(self.model.names))
                     output.append({"bbox": xyxy, "conf": conf, "cls": self.model.names[int(cls)]})
                     if view_img:  # Add bbox to image
                         label = "%s %.2f" % (self.model.names[int(cls)], conf)
@@ -98,7 +97,6 @@ if __name__ == "__main__":
     IMG_FORMATS = 'bmp', 'dng', 'jpeg', 'jpg', 'mpo', 'png', 'tif', 'tiff', 'webp'  # include image suffixes
     VID_FORMATS = 'asf', 'avi', 'gif', 'm4v', 'mkv', 'mov', 'mp4', 'mpeg', 'mpg', 'ts', 'wmv'  # include video suffixes
     
-    
     path = "yolov5/data/images/zidane.jpg"
     model_path = "yolov5/weights/yolov5s.pt"
     source = Path(path).suffix[1:]
@@ -113,4 +111,4 @@ if __name__ == "__main__":
     elif source in VID_FORMATS:                    
         model = YOLOv5(model_path=model_path, device="cpu")
         result = model.video_predict(video_path=path, view_img=True)
- 
+        
