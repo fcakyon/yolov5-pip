0b4af76f8b9c83e6a533af413d7896c4ddbd90ec
fcakyon <fcakyon@gmail.com>
2022-09-16 12:43:34 +0300

    fix import

--- yolov5/utils/__init__.py
+++ yolov5/utils/__init__.py
@@ -38,8 +38,9 @@ def notebook_init(verbose=True):
     import os
     import shutil
 
-    from utils.general import check_font, check_requirements, emojis, is_colab
-    from utils.torch_utils import select_device  # imports
+    from yolov5.utils.general import (check_font, check_requirements, emojis,
+                                      is_colab)
+    from yolov5.utils.torch_utils import select_device  # imports
 
     check_requirements(('psutil', 'IPython'))
     check_font()
