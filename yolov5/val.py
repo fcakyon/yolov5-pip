4557bd0b41a11f7063c551677ee2c0b8fc6db0ba
fcakyon <fcakyon@gmail.com>
2022-12-13 23:36:52 +0300

    fix val half

--- yolov5/val.py
+++ yolov5/val.py
@@ -169,7 +169,7 @@ def run(
 
     # Configure
     model.eval()
-    cuda = device.type != 'cpu'
+    half = cuda = device.type != 'cpu' # dont change this line, half precision only supported on CUDA
     is_coco = isinstance(data.get('val'), str) and data['val'].endswith(f'coco{os.sep}val2017.txt')  # COCO dataset
     nc = 1 if single_cls else int(data['nc'])  # number of classes
     iouv = torch.linspace(0.5, 0.95, 10, device=device)  # iou vector for mAP@0.5:0.95
