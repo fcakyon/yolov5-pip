2816da65283279331a75f72100d0b21bf83bb1cc
fcakyon <fcakyon@gmail.com>
2023-01-27 09:49:05 +0300

    remove deprecated unitests

--- tests/test_yolov5.py
+++ tests/test_yolov5.py
@@ -4,120 +4,8 @@ from tests.test_utils import TestConstants
 
 DEVICE = "cpu"
 
-class TestYolov5FromUltralytics(unittest.TestCase):
+class TestYolov5FromUltralytics(unittest.TestCase):       
     def test_load_model(self):
-        from yolov5 import YOLOv5
-
-        # init model
-        model_path = TestConstants.YOLOV5S_MODEL_PATH
-        yolov5 = YOLOv5(model_path, DEVICE, load_on_init=False)
-        yolov5.load_model()
-
-        # check if loaded
-        self.assertNotEqual(yolov5.model, None)
-
-    def test_load_model_on_init(self):
-        from yolov5 import YOLOv5
-
-        # init model
-        model_path = TestConstants.YOLOV5S_MODEL_PATH
-        yolov5 = YOLOv5(model_path, DEVICE, load_on_init=True)
-
-        # check if loaded
-        self.assertNotEqual(yolov5.model, None)
-
-    def test_predict(self):
-        from PIL import Image
-        from yolov5 import YOLOv5
-
-        # init yolov5s model
-        model_path = TestConstants.YOLOV5S_MODEL_PATH
-        yolov5 = YOLOv5(model_path, DEVICE, load_on_init=True)
-
-        # prepare image
-        image_path = TestConstants.ZIDANE_IMAGE_PATH
-        image = Image.open(image_path)
-
-        # perform inference
-        results = yolov5.predict(image, size=640, augment=False)
-
-        # compare
-        self.assertEqual(results.n, 1)
-        self.assertEqual(len(results.names), 80)
-        self.assertEqual(len(results.pred[0]), 4)
-        
-        # prepare image
-        image = image_path
-
-        # perform inference
-        results = yolov5.predict(image, size=640, augment=True)
-
-        # compare
-        self.assertEqual(results.n, 1)
-        self.assertEqual(len(results.names), 80)
-        self.assertEqual(len(results.pred[0]), 5)
-        
-        # init yolov5l model
-        model_path = TestConstants.YOLOV5L_MODEL_PATH
-        yolov5 = YOLOv5(model_path, DEVICE, load_on_init=True)
-
-        # prepare image
-        image_path = TestConstants.BUS_IMAGE_PATH
-        image = Image.open(image_path)
-        # perform inference
-        results = yolov5.predict(image, size=1280, augment=False)
-
-        # compare
-        self.assertEqual(results.n, 1)
-        self.assertEqual(len(results.names), 80)
-        self.assertEqual(len(results.pred[0]), 6)
-        
-        # prepare image
-        image = image_path
-
-        # perform inference
-        results = yolov5.predict(image, size=1280, augment=False)
-
-        # compare
-        self.assertEqual(results.n, 1)
-        self.assertEqual(len(results.names), 80)
-        self.assertEqual(len(results.pred[0]), 6)
-
-        # init yolov5s model
-        model_path = TestConstants.YOLOV5S_MODEL_PATH
-        yolov5 = YOLOv5(model_path, DEVICE, load_on_init=True)
-
-        # prepare images
-        image_path1 = TestConstants.ZIDANE_IMAGE_PATH
-        image_path2 = TestConstants.BUS_IMAGE_PATH
-        image1 = Image.open(image_path1)
-        image2 = Image.open(image_path2)
-
-        # perform inference with multiple images and test augmentation
-        results = yolov5.predict([image1, image2], size=1280, augment=True)
-
-        # compare
-        self.assertEqual(results.n, 2)
-        self.assertEqual(len(results.names), 80)
-        self.assertEqual(len(results.pred[0]), 4)
-        self.assertEqual(len(results.pred[1]), 5)
-
-        # prepare image
-        image_path1 = TestConstants.ZIDANE_IMAGE_PATH
-        image_path2 = TestConstants.BUS_IMAGE_PATH
-        image1 = image_path1
-        image2 = image_path2
-
-        # perform inference
-        results = yolov5.predict([image1, image2], size=1280, augment=True)
-
-        # compare
-        self.assertEqual(results.n, 2)
-        self.assertEqual(len(results.names), 80)
-        self.assertEqual(len(results.pred[0]), 4)
-        self.assertEqual(len(results.pred[1]), 5)
-        
-    def test_hublike_load_model(self):
         import yolov5
 
         # init model
@@ -127,9 +15,10 @@ class TestYolov5FromUltralytics(unittest.TestCase):
         # check if loaded
         self.assertNotEqual(model, None)
 
-    def test_hublike_predict(self):
-        import yolov5
+    def test_predict(self):
         from PIL import Image
+
+        import yolov5
 
         # init yolov5s model
         model_path = TestConstants.YOLOV5S_MODEL_PATH
@@ -182,120 +71,8 @@ class TestYolov5FromUltralytics(unittest.TestCase):
         self.assertEqual(len(results.pred[1]), 5)
 
 
-class TestYolov5FromHuggingface(unittest.TestCase):
+class TestYolov5FromHuggingface(unittest.TestCase):      
     def test_load_model(self):
-        from yolov5 import YOLOv5
-
-        # init model
-        model_path = TestConstants.YOLOV5S_HUB_ID
-        yolov5 = YOLOv5(model_path, DEVICE, load_on_init=False)
-        yolov5.load_model()
-
-        # check if loaded
-        self.assertNotEqual(yolov5.model, None)
-
-    def test_load_model_on_init(self):
-        from yolov5 import YOLOv5
-
-        # init model
-        model_path = TestConstants.YOLOV5S_HUB_ID
-        yolov5 = YOLOv5(model_path, DEVICE, load_on_init=True)
-
-        # check if loaded
-        self.assertNotEqual(yolov5.model, None)
-
-    def test_predict(self):
-        from PIL import Image
-        from yolov5 import YOLOv5
-
-        # init yolov5s model
-        model_path = TestConstants.YOLOV5S_HUB_ID
-        yolov5 = YOLOv5(model_path, DEVICE, load_on_init=True)
-
-        # prepare image
-        image_path = TestConstants.ZIDANE_IMAGE_PATH
-        image = Image.open(image_path)
-
-        # perform inference
-        results = yolov5.predict(image, size=640, augment=False)
-
-        # compare
-        self.assertEqual(results.n, 1)
-        self.assertEqual(len(results.names), 80)
-        self.assertEqual(len(results.pred[0]), 4)
-        
-        # prepare image
-        image = image_path
-
-        # perform inference
-        results = yolov5.predict(image, size=640, augment=True)
-
-        # compare
-        self.assertEqual(results.n, 1)
-        self.assertEqual(len(results.names), 80)
-        self.assertEqual(len(results.pred[0]), 5)
-        
-        # init yolov5l model
-        model_path = TestConstants.YOLOV5L_MODEL_PATH
-        yolov5 = YOLOv5(model_path, DEVICE, load_on_init=True)
-
-        # prepare image
-        image_path = TestConstants.BUS_IMAGE_PATH
-        image = Image.open(image_path)
-        # perform inference
-        results = yolov5.predict(image, size=1280, augment=False)
-
-        # compare
-        self.assertEqual(results.n, 1)
-        self.assertEqual(len(results.names), 80)
-        self.assertEqual(len(results.pred[0]), 6)
-        
-        # prepare image
-        image = image_path
-
-        # perform inference
-        results = yolov5.predict(image, size=1280, augment=False)
-
-        # compare
-        self.assertEqual(results.n, 1)
-        self.assertEqual(len(results.names), 80)
-        self.assertEqual(len(results.pred[0]), 6)
-
-        # init yolov5s model
-        model_path = TestConstants.YOLOV5S_HUB_ID
-        yolov5 = YOLOv5(model_path, DEVICE, load_on_init=True)
-
-        # prepare images
-        image_path1 = TestConstants.ZIDANE_IMAGE_PATH
-        image_path2 = TestConstants.BUS_IMAGE_PATH
-        image1 = Image.open(image_path1)
-        image2 = Image.open(image_path2)
-
-        # perform inference with multiple images and test augmentation
-        results = yolov5.predict([image1, image2], size=1280, augment=True)
-
-        # compare
-        self.assertEqual(results.n, 2)
-        self.assertEqual(len(results.names), 80)
-        self.assertEqual(len(results.pred[0]), 4)
-        self.assertEqual(len(results.pred[1]), 5)
-
-        # prepare image
-        image_path1 = TestConstants.ZIDANE_IMAGE_PATH
-        image_path2 = TestConstants.BUS_IMAGE_PATH
-        image1 = image_path1
-        image2 = image_path2
-
-        # perform inference
-        results = yolov5.predict([image1, image2], size=1280, augment=True)
-
-        # compare
-        self.assertEqual(results.n, 2)
-        self.assertEqual(len(results.names), 80)
-        self.assertEqual(len(results.pred[0]), 4)
-        self.assertEqual(len(results.pred[1]), 5)
-        
-    def test_hublike_load_model(self):
         import yolov5
 
         # init model
@@ -305,9 +82,10 @@ class TestYolov5FromHuggingface(unittest.TestCase):
         # check if loaded
         self.assertNotEqual(model, None)
 
-    def test_hublike_predict(self):
-        import yolov5
+    def test_predict(self):
         from PIL import Image
+
+        import yolov5
 
         # init yolov5s model
         model_path = TestConstants.YOLOV5S_HUB_ID
